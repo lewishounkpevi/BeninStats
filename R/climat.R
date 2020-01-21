21d2824f7372f7438a80059665987ca65c706d7d
Lewis Hounkpevi <Lorx@users.noreply.github.com>
2020-01-21 22:57:53 +0100

    update R files (#4)

--- R/climat.R
+++ R/climat.R
@@ -1,9 +1,4 @@
-#' Arabidopsis QTL data on gravitropism
-#'
-#' Data from a QTL experiment on gravitropism in
-#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
-#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
-#' increments over eight hours.
+#' Climatologie au Benin
 #'
 #' @docType data
 #'
@@ -11,10 +6,10 @@
 #'
 #' @keywords datasets
 #'
-#' @references Moore et al. (2013) Genetics 195:1077-1086
-#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#' @references Benin Portail des donnees
+#' Derniere publication 2017-2018
 #'
-#' @source \href{https://phenome.jax.org/projects/Moore1b}{QTL Archive}
+#' @source \href{https://benin.opendataforafrica.org/data/#topic=Climatologie}{Climatologie}
 #'
 #' @examples
 #' data(climat)
--- R/pop_by_age.R
+++ R/pop_by_age.R
@@ -1,9 +1,4 @@
-#' Arabidopsis QTL data on gravitropism
-#'
-#' Data from a QTL experiment on gravitropism in
-#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
-#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
-#' increments over eight hours.
+#' Population by Single Year Age Groups - Custom Region - Benin
 #'
 #' @docType data
 #'
@@ -12,7 +7,7 @@
 #' @keywords datasets
 #'
 #' @references Moore et al. (2013) Genetics 195:1077-1086
-#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#' (\href{https://www.census.gov/data-tools/demo/idb/region.php?T=15&RT=0&A=separate&Y=2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018&C=BN&R=0}{PubMed})
 #'
 #' @source \href{https://phenome.jax.org/projects/Moore1b}{QTL Archive}
 #'
